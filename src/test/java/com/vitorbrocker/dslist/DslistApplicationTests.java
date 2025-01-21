c8157306778afe77f807089b18bcfcb6df3e70fa
Vitor Brocker <vitorbrocker123@gmail.com>
2025-01-21 18:46:04 -0300

    Project created

---
+++ src/test/java/com/vitorbrocker/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorbrocker.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
